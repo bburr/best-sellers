cecff4275f193dfa418e73aabc12df36f358c426
Bryan Burr <b.burr@ubreakifix.com>
2022-12-07 11:01:12 -0500

    update isbn test to evaluate successful cases

--- tests/Unit/Rules/IsbnLengthTest.php
+++ tests/Unit/Rules/IsbnLengthTest.php
@@ -9,27 +9,35 @@ class IsbnLengthTest extends TestCase
     public function data(): array
     {
         return [
-            ['123'],
-            ['123123123'],
-            ['12312312312'],
-            ['123123123123'],
-            ['12312312312312'],
+            ['123', true],
+            ['123123123', true],
+            ['1231231231', false],
+            ['12312312312', true],
+            ['123123123123', true],
+            ['1231231231231', false],
+            ['12312312312312', true],
         ];
     }
 
     /**
      * @dataProvider data
      * @param string $value
+     * @param bool $shouldFail
      * @return void
      */
-    public function testRule(string $value): void
+    public function testRule(string $value, bool $shouldFail): void
     {
         $fail = function () {
             throw new \Exception();
         };
 
-        $this->expectException(\Exception::class);
+        try {
+            (new IsbnLength())->__invoke('', $value, $fail);
 
-        (new IsbnLength())->__invoke('', $value, $fail);
+            $this->assertFalse($shouldFail);
+        }
+        catch (\Exception $e) {
+            $this->assertTrue($shouldFail);
+        }
     }
 }
